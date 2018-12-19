7e97739f493b3d7824f8daaa32e5f310048c814f
Yi Qin <yq2247@columbia.edu>
2018-12-18 19:49:04 -0500

    add file path input

--- web/Encoder.py
+++ web/Encoder.py
@@ -1,21 +1,22 @@
 import pandas as pd
 
 
-def IDtoName(dataset):
+def IDtoName(namefilepath, dataset):
     """
-    Input: one dataframe column
+    Input: name file path, one dataframe column
     Return: onde dataframe column
     function: convert the ID to Name for pokemon
     example: test['a'] = IDtoName(test['a'])
     """
-    Names = pd.read_csv("../Data/pokemonNumbers.csv",header=None)
+    Names = pd.read_csv(namefilepath,header=None)
     Names.columns = ['pokemonid', 'name']
     #Names.head()
     dictionary = dict(zip(Names['pokemonid'], Names['name']))
     dataset = dataset.apply(lambda x: dictionary[x])
     return dataset
 
-# #TestCase
+#TestCase
 # test = pd.DataFrame({'a':[1,2,3,45,67]})
-# test['a'] = IDtoName(test['a'])
+# namefilepath = "../Data/pokemonNumbers.csv"
+# test['a'] = IDtoName(namefilepath,test['a'])
 # print(test.head)
